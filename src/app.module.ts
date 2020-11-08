e1c1b3107886ea59c5d38d335e339a65fd4b4d7f
Eduardo Viana <eduardocosta_viana@live.com>
2020-11-08 18:24:02 -0300

    feat: removed unused modules and added new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { FileModule } from './file/file.module';
 import * as databaseOptions from './config/database';
-
 @Module({
   imports: [
     TypeOrmModule.forRoot(databaseOptions),
@@ -13,9 +12,11 @@ import * as databaseOptions from './config/database';
       autoSchemaFile: 'schema.gql',
       sortSchema: true,
       playground: true,
+      context: ({ req }) => ({ req }),
     }),
+    AuthModule,
+    UserModule,
+    FileModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
