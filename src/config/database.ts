dc48357b5b2cbaf9b5633832970b412e839d4191
eduvianna <eduardocosta_viana@live.com>
2021-03-15 19:14:55 -0300

    feat: allow to use DATABASE_URl if exists

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,14 +4,19 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 dotenv.config({ path: 'src/../.env' });
 
+const configDatabase = process.env.DATABASE_URL
+  ? { url: process.env.DATABASE_URL }
+  : {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    };
 const options: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  ...configDatabase,
   logging: true,
-  database: process.env.DB_NAME,
   synchronize: false,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: [path.resolve(__dirname, '..', 'db', 'migrations', '*')],
